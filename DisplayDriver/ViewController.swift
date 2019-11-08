0a517c9b6750906d00d7c2a8b78213f5037d0f93
Vitor Silva <sirmochi45@gmail.com>
2019-11-08 17:26:24 -0300

    Initial Commit

---
+++ DisplayDriver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DisplayDriver
+//
+//  Created by Vitor Silva on 08/11/19.
+//  Copyright © 2019 Vitor Silva. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
